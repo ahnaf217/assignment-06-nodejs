09f0593bd315e4e993f2819386628ce14d4a0f57
ahnaf217 <ahnaflabib56@gmail.com>
2026-09-12 20:17:44 +0600

    added / route

--- assignment.js
+++ assignment.js
@@ -1,7 +1,13 @@
 const http = require("http");
 
 const server = http.createServer((req, res) =>{
+    if(req.url == "/")
+    {
+        res.write("<h1>This is the home page</h1>");
+        res.end();
+    }
     
+
 })
 
 server.listen(5000, () => {
