9938c2d6c45c87176b21faf2e5dbaba004941da8
ahnaf217 <ahnaflabib56@gmail.com>
2026-09-12 20:27:08 +0600

    added descriptions in each of the routes.

--- assignment.js
+++ assignment.js
@@ -4,21 +4,25 @@ const server = http.createServer((req, res) =>{
     if(req.url == "/")
     {
         res.write("<h1>This is the home page</h1>");
+        res.write("<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>");
         res.end();
     }
     else if(req.url == "/blog")
     {
         res.write("<h1>This is the blog page</h1>");
+        res.write("<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>");
         res.end();
     }
     else if(req.url == "/about")
     {
         res.write("<h1>This is the about us page</h1>");
+        res.write("<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>");
         res.end();
     }
     else if(req.url == "/contact")
     {
         res.write("<h1>This is the contact us page</h1>");
+        res.write("<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>");
         res.end();
     }
 
